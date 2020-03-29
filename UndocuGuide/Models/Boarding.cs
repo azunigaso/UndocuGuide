e856654e22e4787b7dc6407c69c6689a6fdbbe86
Alejandro <azunigas@masonlive.gmu.edu>
2020-03-29 13:47:38 -0400

    Adding boarding model

---
+++ UndocuGuide/Models/Boarding.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace UndocuGuide.Models
+{
+    public class Boarding
+    {
+        public string ImagePath { get; set; }
+        public string Header { get; set; }
+        public string Content { get; set; }
+        public View CarouselItem { get; set; }
+    }
+}
