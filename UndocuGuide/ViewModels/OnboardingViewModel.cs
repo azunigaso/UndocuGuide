97a4abd98baa07c1524ef36a9dedb22c87a08402
Alejandro <azunigas@masonlive.gmu.edu>
2020-03-29 13:49:17 -0400

    Setting up UI for onboarding

---
+++ UndocuGuide/ViewModels/OnboardingViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using PropertyChanged;
+using UndocuGuide.Models;
+using UndocuGuide.Views;
+
+namespace UndocuGuide.ViewModels
+{
+    [AddINotifyPropertyChangedInterface]
+    public class OnboardingViewModel
+    {
+        public ObservableCollection<Boarding> Boardings { get; set; }
+        public OnboardingViewModel() {
+            Boardings = new ObservableCollection<Boarding>
+            {
+                new Boarding(){
+                    ImagePath = "onboarding1.svg",
+                    Header = "Sample Header 1",
+                    Content = "Sample text for first onboarding!",
+                    CarouselItem = new WalkthroughItemPage()
+                },
+                new Boarding(){
+                    ImagePath = "onboarding1.svg",
+                    Header = "Sample Header 1",
+                    Content = "Sample text for first onboarding!",
+                    CarouselItem = new WalkthroughItemPage()
+                },
+                new Boarding(){
+                    ImagePath = "onboarding1.svg",
+                    Header = "Sample Header 1",
+                    Content = "Sample text for first onboarding!",
+                    CarouselItem = new WalkthroughItemPage()
+                },
+                new Boarding(){
+                    ImagePath = "onboarding1.svg",
+                    Header = "Sample Header 1",
+                    Content = "Sample text for first onboarding!",
+                    CarouselItem = new WalkthroughItemPage()
+                },
+            };
+        }
+    }
+}
