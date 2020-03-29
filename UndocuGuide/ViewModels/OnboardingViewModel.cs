21d151d342bdf7eee10f41a436edb32c312022a6
Alejandro <azunigas@masonlive.gmu.edu>
2020-03-29 18:47:31 -0400

    add onboarding functionality

--- UndocuGuide/ViewModels/OnboardingViewModel.cs
+++ UndocuGuide/ViewModels/OnboardingViewModel.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows.Input;
 using PropertyChanged;
 using UndocuGuide.Models;
 using UndocuGuide.Views;
+using Xamarin.Forms;
 
 namespace UndocuGuide.ViewModels
 {
@@ -12,6 +14,18 @@ namespace UndocuGuide.ViewModels
     public class OnboardingViewModel
     {
         public ObservableCollection<Boarding> Boardings { get; set; }
+        public int PositionIndex { get; set; }
+        public string NextButtonText 
+        {
+            get 
+            {
+                if (PositionIndex == Boardings.Count - 1)
+                    return "Done";
+                return "Next";
+            }
+        }
+        public ICommand NextCommand { get; set; }
+        public ICommand SkipCommand { get; set; }
         public OnboardingViewModel() {
             Boardings = new ObservableCollection<Boarding>
             {
@@ -40,6 +54,28 @@ namespace UndocuGuide.ViewModels
                     CarouselItem = new WalkthroughItemPage()
                 },
             };
+
+            foreach (var boarding in Boardings) {
+                boarding.CarouselItem.BindingContext = boarding;
+            }
+
+            NextCommand = new Command(Next);
+            SkipCommand = new Command(StartMainPage);
+        }
+        private void StartMainPage() 
+        {
+            Application.Current.MainPage = new MainPage();
+        }
+        private void Next(object obj) 
+        {
+            if (PositionIndex < Boardings.Count - 1)
+            {
+                PositionIndex++;
+            }
+            else 
+            {
+                StartMainPage();
+            }
         }
     }
 }
